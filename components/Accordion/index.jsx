b4dc75aa2815c374e59a670132e583bc138ad693
agjs <hi@aleksandar.xyz>
2020-06-19 12:30:48 +0200

    1 file modified

--- components/Accordion/index.jsx
+++ components/Accordion/index.jsx
@@ -10,6 +10,9 @@ import "./style.scss";
  * if user has selected specific setup
  * e.g. if I choose 3 items from 1 setup, items from other setups should be
  * grayed out with a tooltip explaining why
+ * 
+ * I should add some indicators when something inside of some setup is selected. In the current implementation, that can become confusing because you can select items and minimize the group, not really seeing what is selected and where
+ * 
  */
 
 export default props => {
@@ -50,43 +53,51 @@ export default props => {
         [result.id]: result
       });
     }
-    console.log(selected)
   };
 
+  const handleDeselectAll = () => {
+    setSelected({});
+  };
+
+  const isSelected = Object.keys(selected).length > 0;
+
   return (
-    <ul className="grit42-accordion">
-      {Object.keys(groupedBySetup).map(key => {
-        return (
-          <li
-            className={className("grit42-accordion__expander")}
-            onClick={event => handleExpand(key, groupedBySetup[key])}
-            key={key}
-          >
-            <span className={className("grit42-accordion__expander__title")}>
-              {key}
-            </span>
-            {expanded[key] && expanded[key].expanded && (
-              <ul className={className("grit42-accordion__items")}>
-                {expanded[key].data.map((result, index) => (
-                  <li
-                    className={className("grit42-accordion__items__item", {
-                      "grit42-accordion__items__item--selected":
-                        selected[result.id]
-                    })}
-                    key={`${result.name}-${index}`}
-                    onClick={event => {
-                      event.stopPropagation();
-                      handleSelect(key, result);
-                    }}
-                  >
-                    {result.name}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {isSelected && <button onClick={handleDeselectAll}>Deselect all</button>}
+      <ul className="grit42-accordion">
+        {Object.keys(groupedBySetup).map(key => {
+          return (
+            <li
+              className={className("grit42-accordion__expander")}
+              onClick={event => handleExpand(key, groupedBySetup[key])}
+              key={key}
+            >
+              <span className={className("grit42-accordion__expander__title")}>
+                {key}
+              </span>
+              {expanded[key] && expanded[key].expanded && (
+                <ul className={className("grit42-accordion__items")}>
+                  {expanded[key].data.map((result, index) => (
+                    <li
+                      className={className("grit42-accordion__items__item", {
+                        "grit42-accordion__items__item--selected":
+                          selected[result.id]
+                      })}
+                      key={`${result.name}-${index}`}
+                      onClick={event => {
+                        event.stopPropagation();
+                        handleSelect(key, result);
+                      }}
+                    >
+                      {result.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
